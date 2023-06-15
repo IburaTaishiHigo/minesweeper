ae33d2f8743dc883cde4147398f6aba1c6cac104
IburaTaishiHigo <IburaTaishiHigo@users.noreply.github.com>
2023-06-15 14:43:13 +0000

    deploy: 32a2e203cfa1230a9ee8ff8dc77983a1fb4605fc

---
+++ _next/static/kGZEnhab5EuAth28teaW2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/44639f4e2c6a4b32.css","static/chunks/pages/index-832de1d83a275842.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
